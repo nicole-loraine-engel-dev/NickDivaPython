32ea9c28d6cf03af522e52ae78bdc5f7a59a2595
Nick <engel.nicole2022@gmail.com>
2024-04-30 20:52:52 -0300

    qualquer coisa

---
+++ Tipos_de_dados.py
@@ -0,0 +1,12 @@
+nome = "Nicole Engel"
+idade = 28
+
+nome = "Tyffany"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SC", "RJ", "RS"]
+
+print(BRAZILIAN_STATES)
---
+++ funcao_entrada_saida.py
@@ -0,0 +1,8 @@
+nome = input("Informe o seu nome: ")
+idade = input("imforme a sua idade: ")
+
+print(nome, idade)
+print("teste")
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#")
+print("mudei")
